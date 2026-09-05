cdfcc04fbafa38a5f292905c4b1ad208d15bec88
maantomar2410-glitch <maantomar2410@gmail.com>
2026-09-05 16:34:39 +0530

    Initialize SQLite database and log shot types
    Added SQLite database initialization and logging functionality for tennis shot statistics.

--- app.py
+++ app.py
@@ -1,36 +1,50 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
+
+def init_db():
+    conn = sqlite3.connect('tennis.db')       
+    cursor = conn.cursor()                     
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS shots (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            shot_type TEXT NOT NULL)
+    ''')
+    conn.commit()                             
+    conn.close()                               
+
+init_db()   
 
 stats = {
     "forehand_errors": 0,
     "backhand_errors": 0,
     "serves_in": 0,
     "serves_out": 0,
-    "Aces": 0
+    "aces": 0
 }
 
 @app.route('/')
 def home():
     total = stats["forehand_errors"] + stats["backhand_errors"]
-    winners=stats["Aces"]
-    return render_template("home.html", stats=stats, total=total,winners=winners)
+    return render_template("home.html", stats=stats, total=total , aces=stats["aces"])
+
 
 @app.route('/log', methods=['POST'])
 def log_shot():
-    shot = request.form['shot']       
-    if shot == 'forehand_error':
-        stats["forehand_errors"] += 1
-    elif shot == 'backhand_error':
-        stats["backhand_errors"] += 1
-    elif shot == 'serve_in':
-        stats["serves_in"] += 1
-    elif shot == 'serve_out':
-        stats["serves_out"] += 1
-    elif shot=="Aces":
-        stats["Aces"] += 1
-    return redirect('/')              
+    shot = request.form['shot']         
+
+    conn = sqlite3.connect('tennis.db')  
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO shots (shot_type) VALUES (?)",   
+        (shot,)
+    )
+    conn.commit()                        
+    conn.close()                         
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
